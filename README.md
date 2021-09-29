34ed123d7697ccde9e684366d0b4deeea95176d3
Enes Polat <37534266+enespolat25@users.noreply.github.com>
2021-09-29 15:50:52 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# vuejs-charts
+# Vue eCharts Demo
+
+This repo is the source code created for an article I wrote about how to create charts and graphs in Vue.js. This code shows you how to had both a bar chart and line chart to a Vue.js application.
+
+[You can read my article here](https://medium.com/p/29f943a45d09).
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Run your tests
+```
+npm run test
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <div class="chart-wrapper">
+      <chart :options="chartOptionsBar"></chart>
+    </div>
+    <hr />
+    <div class="chart-wrapper">
+      <chart :options="chartOptionsLine"></chart>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data: () => ({
+    chartOptionsBar: {
+      xAxis: {
+        data: ["Q1", "Q2", "Q3", "Q4"]
+      },
+      yAxis: {
+        type: "value"
+      },
+      series: [
+        {
+          type: "bar",
+          data: [63, 75, 24, 92]
+        }
+      ],
+      title: {
+        text: "Quarterly Sales Results",
+        x: "center",
+        textStyle: {
+          fontSize: 24
+        }
+      },
+      color: ["#127ac2"]
+    },
+    chartOptionsLine: {
+      xAxis: {
+        data: [
+          "Jan",
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep",
+          "Oct",
+          "Nov",
+          "Dec"
+        ]
+      },
+      yAxis: {
+        type: "value"
+      },
+      series: [
+        {
+          type: "line",
+          data: [55, 72, 84, 48, 59, 62, 87, 75, 94, 101, 127, 118]
+        }
+      ],
+      title: {
+        text: "Monthly Stock Prices",
+        x: "center",
+        textStyle: {
+          fontSize: 24
+        }
+      },
+      color: ["#127ac2"]
+    }
+  })
+};
+</script>
+
+<style scoped>
+.chart-wrapper {
+  width: 100%;
+  height: 700px;
+}
+.echarts {
+  width: 100%;
+  height: 100%;
+}
+</style>
